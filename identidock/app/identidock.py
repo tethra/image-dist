ade1f0d4dd7cee1d6da6868e1ce7926a936a2f48
tethra <inphyyid+community@gmail.com>
2017-07-25 18:44:49 +0900

    Update identidock.py

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -1,39 +1,38 @@
-from flask import Flask, Response, request
-import requests
 import hashlib
+
+from flask import Flask, Response, request
+import html
 import redis
+import requests
+
 
 app = Flask(__name__)
 cache = redis.StrictRedis(host='redis', port=6379, db=0)
 salt = "UNIQUE_SALT"
 default_name = 'Joe Bloggs'
 
-
 @app.route('/', methods=['GET', 'POST'])
 def mainpage():
-
     name = default_name
     if request.method == 'POST':
-        name = request.form['name']
+        name = html.escape(request.form['name'], quote=True)
 
     salted_name = salt + name
     name_hash = hashlib.sha256(salted_name.encode()).hexdigest()
     header = '<html><head><title>Identidock</title></head><body>'
     body = '''<form method="POST">
-              Hello <input type="text" name="name" value="{0}">
-              <input type="submit" value="submit">
-              </form>
-              <p>You look like a:
-              <img src="/monster/{1}"/>
-              '''.format(name, name_hash)
+    Hello <input type="text" name="name" value="{0}">
+    <input type="submit" value="submit">
+    </form>
+    <p>You look like a:
+    <img src="/monster/{1}"/>
+    '''.format(name, name_hash)
     footer = '</body></html>'
-
     return header + body + footer
-
 
 @app.route('/monster/<name>')
 def get_identicon(name):
-
+    name = html.escape(name, quote=True)
     image = cache.get(name)
     if image is None:
         print ("Cache miss", flush=True)
